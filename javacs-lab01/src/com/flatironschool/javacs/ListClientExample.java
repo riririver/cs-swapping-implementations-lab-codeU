f4a9c83f7df25289dde4d751574b526612e11382
Riyana Basu <riyanabasu@gmail.com>
2016-05-31 22:18:46 -0700

    Lab 01

--- javacs-lab01/src/com/flatironschool/javacs/ListClientExample.java
+++ javacs-lab01/src/com/flatironschool/javacs/ListClientExample.java
@@ -1,13 +1,14 @@
 package com.flatironschool.javacs;
 
-import java.util.LinkedList;
+//import java.util.LinkedList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ListClientExample {
 	private List list;
 	
 	public ListClientExample() {
-		list = new LinkedList();
+		list = new ArrayList();
 	}
 
 	public List getList() {
